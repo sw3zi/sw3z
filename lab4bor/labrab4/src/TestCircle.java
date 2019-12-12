a2ce34473358f2dc7bf6dc4d298a2e2e901a6578
Nikita Borisov <56160237+sw3zi@users.noreply.github.com>
2019-12-12 17:50:36 +0300

    Add files via upload

---
+++ lab4bor/labrab4/src/TestCircle.java
@@ -0,0 +1,15 @@
+import java.lang.*;
+import java.util.Scanner;
+
+public class TestCircle {
+    public static void main(String[] args) {
+        double r;
+        Circle k1 = new Circle(3.1, 4.1, 5.1, "black");
+        System.out.println("ДО = " + k1.getLength() + "см\n");
+        Scanner source = new Scanner(System.in);
+        System.out.println("Введите радиус ");
+        r = source.nextDouble();
+        k1.setR(r);
+        System.out.println("\nДлина окружности = " + k1.getLength() + "см");
+    }
+}
