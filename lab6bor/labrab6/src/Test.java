a1afbb4a82a35272042d01bb4429da33d1406b89
Nikita Borisov <56160237+sw3zi@users.noreply.github.com>
2019-12-12 17:52:30 +0300

    Add files via upload

---
+++ lab6bor/labrab6/src/Test.java
@@ -0,0 +1,11 @@
+public class Test {
+    public static void main(String[] args) {
+        Car c1 = new Car("Dutsyn", "r1", "GT", 2300000, 2015);
+        Car c2 = new Car("Lada", "Granta", "Cross", 40000, 1999);
+        Car c3 = new Car("Shkoda", "JX", "Standart", 230000, 1111);
+        c1.println();
+        c2.println();
+        c3.println();
+    }
+}
+
